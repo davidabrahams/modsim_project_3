2e3a67e688176f221754babc3f2ab89f31b5194f
David Abrahams <david.abrahams@students.olin.edu>
2014-11-17 19:34:40 -0500

    reorganized and restructured code

--- orbit.m
+++ orbit.m
@@ -1,12 +1,15 @@
 function [T Trajectory] = orbit(v_initial)
 
+%set the constants
+m_planet = 5.972e24; %kg
 r_planet = 6378137 %radius of planet Earth (m)
 angular_velocity_planet = 7.2921150e-5; %angular velocity of planet Earth (rad/s)
 equatorial_speed = r_planet * angular_velocity_planet
 
-[T Trajectory] = trajectory(v_initial, r_planet, equatorial_speed);
-X = Trajectory(:, 1)
-Y = Trajectory(:, 2)
+%evaluate the trajectory of the projectile
+[T Trajectory] = trajectory(v_initial, r_planet, equatorial_speed, m_planet);
+X = Trajectory(:, 1);
+Y = Trajectory(:, 2);
 plot(X, Y);
 
 end
--- trajectory.m
+++ trajectory.m
@@ -1,10 +1,37 @@
-function [T Trajectory] = trajectory(v_initial, r_planet, equatorial_speed)
+function [T Trajectory] = trajectory(v_initial, r_planet, equatorial_speed, m_planet)
 
 v_x = equatorial_speed;
 v_y = v_initial;
 x = 0;
 y = r_planet;
-[T, Trajectory] = ode45(@projectile, [0 100], [x y v_x v_y]);
+[T, Trajectory] = ode45(@derivs, [0 100], [x y v_x v_y]);
+
+    function res = derivs(t, W)
+    % t:    the time
+    % W:    the current position and velocity [x; y; dx/dt; dy/dt]
+    % res:  a vector [dx/dt; dy/dt; d2x/dt; d2y/dt]
+
+    P = W(1:2); %the current position [x y]
+    V = W(3:4); %the current velocity [dx/dt dy/dt]
+
+    dPdt = V; % change in position is velocity
+    dVdt = acceleration(P, m_planet);
+
+    res = [dPdt; dVdt];
+end
+
+end
+
+function res = acceleration(P, m_planet)
+
+G = 6.67384e-11;
+x = P(1);
+y = P(2);
+
+a_grav = G * m_planet / sqrt(x^2 + y^2);
+
+unit_vector = [x; y] ./ norm([x y]);
+res = -a_grav * unit_vector;
 
 end
 
